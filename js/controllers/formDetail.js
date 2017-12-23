6e9d920e9a7a47242427f35e6c8682129cae3d38
Al Dynak <adynak@gmail.com>
2017-12-22 16:55:55 -0800

    beware of dev vs prod in dataservice

--- js/controllers/formDetail.js
+++ js/controllers/formDetail.js
@@ -3,6 +3,102 @@ searchForms.controller('FormDetailController', ['$scope', '$uibModal', '$uibModa
 
         $scope.selectedRow = selectedRow;
         $scope.prompts = txtReviewForm;
+        console.log(selectedRow.formFields);
+
+        var gridData = selectedRow.formFields;
+
+        $scope.gridOptions = {
+            enableFiltering: false,
+            enableSorting: false,
+            data: gridData,
+            enableRowSelection: true,
+            multiSelect: false,
+            cellTooltip: true,
+            enableRowHeaderSelection: false,
+            onRegisterApi: function(gridApi){ 
+                $scope.gridApi = gridApi;
+            },
+            appScopeProvider: $scope.myAppScopeProvider,
+            rowTemplate: "<div ng-mousedown=\"grid.appScope.showInfo(row,$event)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell></div>",
+            columnDefs: [
+                {
+                    name: 'horizontal',
+                    displayName: $scope.prompts.gridColumnHorizontal,
+                    cellClass: 'grid-align-right',
+                    headerCellClass: 'grid-header-align-right',
+                    enableColumnMenu: false,
+                    width: '3%'
+                },
+                {
+                    name: 'vertical',
+                    displayName: $scope.prompts.gridColumnVertical,
+                    cellClass: 'grid-align-right',                    
+                    headerCellClass: 'grid-header-align-right',
+                    enableColumnMenu: false ,
+                    width: '3%'
+                },
+                {
+                    name: 'fieldNumber',
+                    displayName: $scope.prompts.gridColumnFieldNumber,
+                    cellClass: 'grid-align-right',
+                    // headerCellClass: 'grid-header-align-right',
+                    enableColumnMenu: false,
+                    width: '5%'                    
+                },
+                { 
+                    name: 'width',
+                    displayName: $scope.prompts.gridColumnWidth, 
+                    cellClass: 'grid-align-right',                    
+                    // headerCellClass: 'grid-header-align-right',
+                    enableColumnMenu: false,
+                    width: '6%'                    
+                },
+                {
+                    name: 'conversion',
+                    displayName: $scope.prompts.gridColumnConversion,
+                    cellClass: 'grid-align-left',
+                    // headerCellClass: 'grid-header-align-left',
+                    enableColumnMenu: false,
+                    cellTemplate: 'views/description.html',
+                    width: '5%'                    
+
+                },
+                {
+                    name: 'justification',
+                    displayName: $scope.prompts.gridColumnJustification,
+                    cellClass: 'grid-align-left',
+                    // headerCellClass: 'grid-header-align-left',
+                    enableColumnMenu: false,
+                    cellTemplate: 'views/comment.html',
+                    width: '5%'                                        
+                },
+                {
+                    name: 'description',
+                    displayName: $scope.prompts.gridColumnDescription,
+                    cellClass: 'grid-align-left',
+                    // headerCellClass: 'grid-header-align-left',
+                    enableColumnMenu: false,
+                },
+                {
+                    name: 'default',
+                    displayName: $scope.prompts.gridColumnDefault,
+                    cellClass: 'grid-align-left',
+                    // headerCellClass: 'grid-header-align-left',
+                    enableColumnMenu: false,
+                    cellTemplate: 'views/comment.html',
+                    width: '10%'
+                },
+                {
+                    name: 'printField',
+                    displayName: $scope.prompts.gridColumnPrintField,
+                    cellClass: 'grid-align-left',
+                    // headerCellClass: 'grid-header-align-left',
+                    enableColumnMenu: false,
+                }                                
+            ]
+        };
+
+
 
         $scope.doesJsonHaveFormFields = function(){
             return selectedRow.formFields.length;
